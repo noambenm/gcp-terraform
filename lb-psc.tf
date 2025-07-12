e2cdb6ed384d92778c6bb13c880ec29ecc1e59a4
noambenm <noam@mdch-lab.dev>
2025-07-12 14:01:13 +0300

    Disable CDN for default backend in edge_lb module

--- lb-psc.tf
+++ lb-psc.tf
@@ -24,6 +24,7 @@ module "edge_lb" {
       protocol     = "HTTP"
       port         = 80
       security_policy = google_compute_security_policy.waf.id
+      enable_cdn = false
 
       groups = [{
         group                 = google_compute_region_network_endpoint_group.psc_neg.id
