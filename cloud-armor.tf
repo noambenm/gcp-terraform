568974b1a0e85b0aa0f48450757a94c9759e2843
noambenm <noam@mdch-lab.dev>
2025-07-13 21:56:11 +0300

    changed the origin to IL

--- cloud-armor.tf
+++ cloud-armor.tf
@@ -1,17 +1,3 @@
-locals {
-  # Cloudflare IPv4 / IPv6 ranges – update periodically
-  cf_ipv4 = [
-    "173.245.48.0/20", "103.21.244.0/22", "104.16.0.0/13",
-    "131.0.72.0/22", "190.93.240.0/20", "108.162.192.0/18"
-  ]
-
-  cf_ipv6 = [
-    "2400:cb00::/32", "2606:4700::/32", "2803:f800::/32"
-  ]
-
-  allowed_ips = concat(local.cf_ipv4, local.cf_ipv6, [var.home_ip],)
-}
-
 resource "google_compute_security_policy" "waf" {
   name        = "edge-waf"
   description = "Allow Cloudflare IPs, block everything else"
@@ -22,9 +8,8 @@ resource "google_compute_security_policy" "waf" {
     action   = "allow"
 
     match {
-      versioned_expr = "SRC_IPS_V1"
-      config {
-        src_ip_ranges = local.allowed_ips
+      expr {
+        expression = "source.region_code == 'IL'"
       }
     }
   }
